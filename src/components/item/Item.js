b636797f93e31e32fd8ab1b23ac3eda1c9374a41
Michael Jungo <michaeljungo92@gmail.com>
2016-12-01 17:00:34 +0100

    Add flow

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,7 +1,8 @@
+/* @flow */
 import React from 'react';
 import './item.css';
 
-const Item = ({ comment, command }) => (
+const Item = ({ comment, command }: { comment: string, command: string }) => (
   <div className="item">
     <div className="comment">{`# ${comment}`}</div>
     <div className="command">{command}</div>
--- src/components/item/ItemList.js
+++ src/components/item/ItemList.js
@@ -1,3 +1,4 @@
+/* @flow */
 import React from 'react';
 import Item from './Item';
 
