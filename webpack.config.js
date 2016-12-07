fd5d4de12b0cf745b3ab08dccc589412f35b1bcf
Michael Jungo <michaeljungo92@gmail.com>
2016-12-07 18:40:00 +0100

    Use webpack-blocks
    The Webpack 2 packages are currently not on the npm registry, so they
    need to be linked with `yarn link [package-name]`.
    Additionally the `env` function seems to generate wrong webpack configs.

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const {
+  addPlugins,
+  createConfig,
+  entryPoint,
+  env,
+  setOutput,
+  sourceMaps,
+  webpack
+} = require('@webpack-blocks/webpack2');
+const babel = require('@webpack-blocks/babel6');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const devServer = require('@webpack-blocks/dev-server2');
+const extractText = require('@webpack-blocks/extract-text2');
+
+module.exports = createConfig([
+  entryPoint('./src/main.js'),
+  setOutput('./dist/bundle.js'),
+  babel(),
+  env('development', [
+    sourceMaps(),
+    devServer()
+  ]),
+  env('production', [
+    extractText('style.css'),
+    addPlugins([
+      new CopyWebpackPlugin([{ from: './index.html' }]),
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+        debug: false
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        },
+        output: {
+          comments: false
+        },
+        sourceMap: false
+      }),
+    ])
+  ])
+]);
