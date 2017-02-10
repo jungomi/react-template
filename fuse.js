daad43ed889680a55b06d7081e3087c2f4f46c07
Michael Jungo <michaeljungo92@gmail.com>
2017-02-10 22:28:21 +0100

    Use fuse-box instead of webpack

---
+++ fuse.js
@@ -0,0 +1,40 @@
+/* eslint-disable new-cap */
+const fsbx = require('fuse-box');
+const fs = require('fs-extra');
+
+const fb = new fsbx.FuseBox({
+  homeDir: 'src/',
+  outFile: 'dist/bundle.js',
+  sourceMap: {
+    bundleReference: 'sourcemaps.js.map',
+    outFile: 'dist/sourcemaps.js.map',
+  },
+  plugins: [
+    fsbx.CSSPlugin(),
+    fsbx.BabelPlugin({
+      config: {
+        sourceMaps: true,
+        presets: [
+          ['env', {
+            'targets': {
+              'browsers': ['last 2 versions']
+            }
+          }],
+          'react'
+        ],
+        plugins: [
+          'transform-runtime',
+          'transform-flow-strip-types',
+          'react-hot-loader/babel'
+        ]
+      }
+    })
+  ]
+});
+
+fs.copySync('index.html', 'dist/index.html');
+if (process.env.NODE_ENV === 'production') {
+  fb.bundle('>main.js');
+} else {
+  fb.devServer('>main.dev.js', { port: 8080 });
+}
