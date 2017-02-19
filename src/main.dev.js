d975567888ccc85df7100658cddd82568fa777fa
Michael Jungo <michaeljungo92@gmail.com>
2017-02-19 22:20:34 +0100

    Use async/await

--- src/main.dev.js
+++ src/main.dev.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import './main.css';
 
-function init() {
-  import('./App').then(({ default: App }) => {
-    ReactDOM.render(
-      <AppContainer>
-        <App />
-      </AppContainer>,
-      document.getElementById('app')
-    );
-  });
+async function init() {
+  const { default: App } = await import('./App');
+  ReactDOM.render(
+    <AppContainer>
+      <App />
+    </AppContainer>,
+    document.getElementById('app')
+  );
 }
 
 if (module.hot) {
