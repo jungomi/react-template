bcb9c50dda843a2894a72919027bfb6e99364ecd
Michael Jungo <michaeljungo92@gmail.com>
2017-02-19 19:21:34 +0100

    Remove duplicate code in dev entry

--- src/main.dev.js
+++ src/main.dev.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import App from './App';
 import './main.css';
 
-ReactDOM.render(
-  <AppContainer>
-    <App />
-  </AppContainer>,
-  document.getElementById('app')
-);
-
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    import('./App').then(({ default: NewApp }) => {
-      ReactDOM.render(
-        <AppContainer>
-          <NewApp />
-        </AppContainer>,
-        document.getElementById('app')
-      );
-    });
+function init() {
+  import('./App').then(({ default: App }) => {
+    ReactDOM.render(
+      <AppContainer>
+        <App />
+      </AppContainer>,
+      document.getElementById('app')
+    );
   });
 }
+
+if (module.hot) {
+  module.hot.accept('./App', init);
+}
+
+init();
