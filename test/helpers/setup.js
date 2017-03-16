50302a332d8251aecc179d587c236a161f9bed0f
Michael Jungo <michaeljungo92@gmail.com>
2017-03-16 03:09:26 +0100

    Fix transpiling ES modules for tests

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -1,5 +1,7 @@
 require('ignore-styles');
-require('babel-register');
+require('babel-register')({
+  presets: [['env', { targets: { node: 'current' } }]]
+});
 
 global.document = require('jsdom').jsdom('<body></body>');
 global.window = document.defaultView;
